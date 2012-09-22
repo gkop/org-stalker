80bfc48793975b884590fa371dafb6d72bb76ece
Gabe Kopley <gabe@coshx.com>
2012-09-22 16:05:19 -0700

    Concurrent API queries w/ logging

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source "http://rubygems.org"
 
 gem "goliath", :git => "git://github.com/postrank-labs/goliath.git"
+gem "em-http-request"
 gem "haml"
+gem "json"
 gem "tilt"
 gem "coffee-script"
 gem "pry"
--- hubgit.rb
+++ hubgit.rb
@@ -1,9 +1,35 @@
 require 'bundler/setup'
 Bundler.require
+require 'em-synchrony/em-http'
 
 class Hubgit < Goliath::API
 	# render templated files from ./views
 	include Goliath::Rack::Templates
+
+  def show(account)
+    env['account'] = account
+    url = "https://api.github.com/orgs/#{account}/members"
+
+    http = logged_api_call(:organization, 1) do
+      EM::HttpRequest.new(url).get
+    end
+
+    if http.response
+      users = JSON.parse(http.response)
+      multi = EM::Synchrony::Multi.new
+
+      users.each do |u|
+        multi.add(u['login'], EM::HttpRequest.new(u['url']).aget)
+      end
+      res = logged_api_call(:users, users.size) do
+        multi.perform
+      end
+      env['users'] = multi.responses[:callback].map do |response|
+        JSON.parse(response[1].response)
+      end
+      [200, {}, haml(:show)]
+    end
+  end
 
   def response(env)
     if env['REQUEST_PATH'] =~ /\A\/?\z/
@@ -14,8 +40,21 @@ class Hubgit < Goliath::API
       [200, {'Content-Type' => 'application/javascript'}, coffee(:app)]
     elsif (r = env['REQUEST_PATH'].match /\A\/([^\/]*)/).size > 1
       # show the org or user
-      @account = r[1]
-      [200, {}, haml(:show)]
+      show(r[1])
     end
+  end
+
+  private
+  def logged_api_call(name, n)
+    t0 = Time.now
+    r = yield
+    duration = Time.now - t0
+    log_api_call(name, n, duration)
+    r
+  end
+
+  def log_api_call(name, n, duration)
+    env['api_calls'] ||= []
+    env['api_calls'] << {:name => name, :num_requests => n, :total_time => duration}
   end
 end
